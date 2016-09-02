e5ea8ece0eab984f6705e8551e0c10d06b532285
Pato <pevargas@valdosta.edu>
2016-09-01 22:01:30 -0400

    checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,19 @@ class OrdersController < ApplicationController
 	  session[:order_id] = nil
 	  redirect_to root_path, :notice => "Basket emptied successfully."
 	end
+	def checkout
+	  @order = Shoppe::Order.find(current_order.id)
+	end
+	def payment
+	  if request.post?
+	    redirect_to checkout_confirmation_path
+	  end
+	end
+	def confirmation
+	  if request.post?
+	    current_order.confirm!
+	    session[:order_id] = nil
+	    redirect_to root_path, :notice => "Order has been placed successfully!"
+	  end
+	end
 end
