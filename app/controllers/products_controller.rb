53f510e8b0c3ff031f2703cb31c27ccf97ababbf
Pato <pevargas@valdosta.edu>
2016-09-01 21:54:26 -0400

    basket set up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,11 @@ class ProductsController < ApplicationController
     @products = Shoppe::Product.root.ordered.includes(:product_categories, :variants)
     @products = @products.group_by(&:product_category)
   end
-
+  def buy
+	  @product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+	  current_order.order_items.add_item(@product, 1)
+	  redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
+  end
   def show
     @product = Shoppe::Product.root.find_by_permalink(params[:permalink])
   end
