e91c3ce55ebb4678c1e632b2165b9a264440d7ce
Pato <pevargas@valdosta.edu>
2016-09-25 18:11:22 -0400

    everything is up to date

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   root 'welcome#index' , as: 'welcome'
   root :to => 'welcome#index'
 
+  #shoppe gem admin panel
+  get 'shoppe' , as: 'shoppe'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
